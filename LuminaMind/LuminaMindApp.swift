45fbd46a330e0e3a412d8b434f1901952956297b
- <->
2023-06-06 23:24:40 +0700

    Initial Commit

---
+++ LuminaMind/LuminaMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaMindApp.swift
+//  LuminaMind
+//
+//  Created by Abner Edgar on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LuminaMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
